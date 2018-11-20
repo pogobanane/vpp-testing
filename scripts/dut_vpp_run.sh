ffc0522adf6c369326daaec8b0491314183d0e93
Pogobanane <aenderboy@gmx.de>
2018-11-20 17:41:35 +0100

    add pos magic to dut_vpp_run.sh

--- scripts/dut_vpp_run.sh
+++ scripts/dut_vpp_run.sh
@@ -1,8 +1,61 @@
 #!/bin/bash
 # expects a dut_test*.yaml
+# expects the ba-okelmann git to be checked out at ~/ba-okelmann
+GITDIR="/root/ba-okelmann"
+cd "$GITDIR"
 
+# exit on error
+set -e
+# log every command
+set -x
 
+echo $(pos_get_variable hostname)
+
+# write back some information about the host that might be of interest for the
+# evaluation
+pos_set_variable host/kernel-name $(uname -s)
+pos_set_variable host/kernel-release $(uname -r)
+pos_set_variable host/kernel-version $(uname -v)
+pos_set_variable host/os $(uname -o)
+pos_set_variable host/machine $(uname -m)
+
+# write back the hash of the exact git commit used for reference later
+cd "$GITDIR"
+pos_set_variable git/commit-hash $(git rev-parse --verify HEAD)
+cd "$GITDIR/vpp"
+pos_set_variable git/vpp-commit-hash $(git rev-parse --verify HEAD)
+cd "$GITDIR"
+
+# disable turbo boost
+echo 1 >   /sys/devices/system/cpu/intel_pstate/no_turbo
+
+# set frequency
+echo $(pos_get_variable cpu-freq) > /sys/devices/system/cpu/intel_pstate/max_perf_pct
+echo $(pos_get_variable cpu-freq) > /sys/devices/system/cpu/intel_pstate/min_perf_pct
+
+# set clean up vpp
 rm -f /dev/shm/db /dev/shm/global_vm /dev/shm/vpe-api
 #modprobe uio_pci_generic
 
-vpp -c $(pos_get_variable vpp/config)
+# load some variables
+VPP_CONFIG=$(pos_get_variable vpp/config)
+
+echo 'Done setting up'
+pos_sync
+echo 'sync done'
+
+echo "Starting test"
+
+# run libmoon in background using pos_run
+# pos_run COMMMAND_ID -- COMMAND
+pos_run duttest1 -- /usr/bin/vpp -c $VPP_CONFIG
+
+# wait for test done signal
+pos_sync
+echo "Stopped test $STEP"
+
+# kill the process started with pos_run
+# command/stdout/stderr are uploaded automatically
+pos_kill tuttest1
+
+echo "all done"
