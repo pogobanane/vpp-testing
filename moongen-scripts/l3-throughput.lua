4c15af94b6e7d2993d8bcdb32b27d2dd589ea0fc
Pogobanane <aenderboy@gmx.de>
2019-01-31 20:58:02 +0100

    extract funcitons to other lua file

--- moongen-scripts/l3-throughput.lua
+++ moongen-scripts/l3-throughput.lua
@@ -7,8 +7,8 @@ local hist   = require "histogram"
 local log    = require "log"
 local random = math.random
 
--- package.path = package.path .. ";./vpputil.lua"
--- local vpputil = require "vpputil"
+package.path = package.path .. ";./throughput-util.lua"
+require "throughput-util"
 
 function configure(parser)
   parser:description("Generates CBR traffic with hardware rate control")
@@ -42,43 +42,16 @@ function master(args)
   mg.waitForTasks()
 end
 
-function logThroughput(txCtr, rxCtr, file)
-  log:info(("Saving throughput to '%s'"):format(file))
-  file = io.open(file, "w+")
-  file:write("devDesc,mpps_avg,mpps_stdDev,mbit_avg,mbit_stdDev,wirembit_avg,bytes_total,packets_total\n")
-  file:write(("txCtr,%f,%f,%f,%f,%f,%f,%f\n"):format(
-    txCtr.mpps.avg, txCtr.mpps.stdDev,
-    txCtr.mbit.avg, txCtr.mbit.stdDev,
-    txCtr.wireMbit.avg,
-    txCtr.total, txCtr.totalBytes
-  ))
-  file:write(("rxCtr,%f,%f,%f,%f,%f,%f,%f\n"):format(
-    rxCtr.mpps.avg, rxCtr.mpps.stdDev,
-    rxCtr.mbit.avg, rxCtr.mbit.stdDev,
-    rxCtr.wireMbit.avg,
-    rxCtr.total, rxCtr.totalBytes
-  ))
-  file:close()
-end
-
-function logLatency(hist, file)
-  log:info(("Saving latency to '%s'"):format(file))
-  file = io.open(file, "w+")
-  file:write("samples,average_ns,stdDev_ns,quartile_25th,quartile_50th,quartile_75th\n")
-  file:write(("%u,%f,%f,%f,%f,%f\n"):format(hist.numSamples, hist.avg, hist.stdDev, unpack(hist.quarts)))
-  file:close()
-end
-
 local function fillUdpPacket(buf, eth_src, eth_dst, ip_src, ip_dst, len)
-	buf:getUdpPacket():fill{
-		ethSrc = eth_src,
-		ethDst = eth_dst,
-		ip4Src = ip_src,
-		ip4Dst = ip_dst,
-		udpSrc = 1,
-		udpDst = 2,
-		pktLength = len
-	}
+  buf:getUdpPacket():fill{
+    ethSrc = eth_src,
+    ethDst = eth_dst,
+    ip4Src = ip_src,
+    ip4Dst = ip_dst,
+    udpSrc = 1,
+    udpDst = 2,
+    pktLength = len
+  }
 end
 
 local function fillUdpPacketFlows(buf, eth_src, eth_dst, ip_src_nr, ip_dst, flows, len)
@@ -142,29 +115,6 @@ function sendSimple(bufs, txQueue, txCtr, rxCtr, pktSize)
   end
 end
 
--- unfinished
--- bufs: getudppacket fills!
--- multiple flows from devices or to devices?
--- to deviecs -> have to fill arps for them
--- from devices -> overhead from arp filling
-function sendIPs(bufs, txQueue, txCtr, rxCtr, pktSize, baseIP, flows)
-  local counter = 0
-  local baseIpNr = parseIPAddress(baseIP)
-  while mg.running() do
-    bufs:alloc(pktSize)
-    for _, buf in ipairs(bufs) do
-      local pkt = buf:getUdpPacket()
-      pkt.ip4.dst:set(baseMacNr + counter)
-      counter = incAndWrap(counter, flows) -- starts to increment the leftmost byte to test subnets when flows is not max value but valueCount
-    end
-    -- UDP checksums are optional, so using just IPv4 checksums would be sufficient here
-    bufs:offloadUdpChecksums()
-    txQueue:send(bufs)
-    txCtr:update()
-    rxCtr:update()
-  end
-end
-
 function loadSlave(txQueue, rxDev, eth_src, eth_dst, ip_src, ip_dst, pktSize, flows, file)
   local ip_src_nr = parseIPAddress(ip_src)
   local mem
@@ -209,6 +159,7 @@ function txWarmup(recTask, txQueue, eth_src, eth_dst, ip_src, ip_dst, pktSize)
   mg.sleepMillis(1000) -- ensure that waitWarmup is listening
   while recTask:isRunning() do
     bufs:alloc(pktSize)
+    bufs:offloadUdpChecksums()
     txQueue:send(bufs)
     log:info("warmup packet sent")
     mg.sleepMillis(1500)
---
+++ moongen-scripts/throughput-util.lua
@@ -0,0 +1,50 @@
+
+function logThroughput(txCtr, rxCtr, file)
+  log:info(("Saving throughput to '%s'"):format(file))
+  file = io.open(file, "w+")
+  file:write("devDesc,mpps_avg,mpps_stdDev,mbit_avg,mbit_stdDev,wirembit_avg,bytes_total,packets_total\n")
+  file:write(("txCtr,%f,%f,%f,%f,%f,%f,%f\n"):format(
+    txCtr.mpps.avg, txCtr.mpps.stdDev,
+    txCtr.mbit.avg, txCtr.mbit.stdDev,
+    txCtr.wireMbit.avg,
+    txCtr.total, txCtr.totalBytes
+  ))
+  file:write(("rxCtr,%f,%f,%f,%f,%f,%f,%f\n"):format(
+    rxCtr.mpps.avg, rxCtr.mpps.stdDev,
+    rxCtr.mbit.avg, rxCtr.mbit.stdDev,
+    rxCtr.wireMbit.avg,
+    rxCtr.total, rxCtr.totalBytes
+  ))
+  file:close()
+end
+
+function logLatency(hist, file)
+  log:info(("Saving latency to '%s'"):format(file))
+  file = io.open(file, "w+")
+  file:write("samples,average_ns,stdDev_ns,quartile_25th,quartile_50th,quartile_75th\n")
+  file:write(("%u,%f,%f,%f,%f,%f\n"):format(hist.numSamples, hist.avg, hist.stdDev, unpack(hist.quarts)))
+  file:close()
+end
+
+-- unfinished
+-- bufs: getudppacket fills!
+-- multiple flows from devices or to devices?
+-- to deviecs -> have to fill arps for them
+-- from devices -> overhead from arp filling
+function sendIPs(bufs, txQueue, txCtr, rxCtr, pktSize, baseIP, flows)
+  local counter = 0
+  local baseIpNr = parseIPAddress(baseIP)
+  while mg.running() do
+    bufs:alloc(pktSize)
+    for _, buf in ipairs(bufs) do
+      local pkt = buf:getUdpPacket()
+      pkt.ip4.dst:set(baseMacNr + counter)
+      counter = incAndWrap(counter, flows) -- starts to increment the leftmost byte to test subnets when flows is not max value but valueCount
+    end
+    -- UDP checksums are optional, so using just IPv4 checksums would be sufficient here
+    bufs:offloadUdpChecksums()
+    txQueue:send(bufs)
+    txCtr:update()
+    rxCtr:update()
+  end
+end
