e941420506af352a0c7fb16b4462ce417d8d7c7d
Pogobanane <aenderboy@gmx.de>
2019-02-09 23:34:47 +0100

    create broken vxlan.sh

---
+++ scripts/vpp_tests/vxlan-encapsulated.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# expects to be run from the ba-okelmann project root
+
+# expects $3: INT_SRC_PCI; $4: INT_DST_PCI
+INT_SRC_PCI=$3
+INT_DST_PCI=$4
+
+# expects $1: INT_SRC; $2: INT_DST
+source scripts/vpp_tests/functions.sh
+
+bdid=7 # bridge domain id
+exec="set int state $INT_SRC up
+set int state $INT_DST up
+
+create vxlan tunnel src 10.1.0.2 dst 10.2.0.2 vni $bdid instance $bdid
+create bridge-domain $bdid learn 0 uu-flood 0 flood 0
+
+set int l2 bridge vxlan_tunnel$bdid $bdid
+set int l2 bridge $INT_SRC $bdid
+set int l2 bridge $INT_DST $bdid
+
+set int mac address $INT_SRC $INT_SRC_MAC
+set int mac address $INT_DST $INT_DST_MAC
+
+l2fib add $MAC_SRC $bdid $INT_SRC
+l2fib add $MAC_DST $bdid $INT_DST
+"
+
+config_workers="
+unix {
+	exec $VPP_EXEC
+	cli-listen $VPP_CLI_LISTEN
+}
+cpu {
+	main-core 1
+	corelist-workers 4-7
+}
+dpdk {
+	socket-mem 1024,1024
+	dev ${INT_SRC_PCI} {
+		num-rx-queues 2
+	}
+	dev ${INT_DST_PCI} {
+		num-rx-queues 2
+	}
+}
+"
+
+test_vpp_with "$config_workers" "$exec"
